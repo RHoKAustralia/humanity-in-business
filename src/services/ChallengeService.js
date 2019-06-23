f38773989d07efdf394efb65bdd360d2e28e0944
andytan2624 <andy.tan2624@gmail.com>
2019-06-23 12:26:31 +1000

    Get challenge data and get all skills

--- src/services/ChallengeService.js
+++ src/services/ChallengeService.js
@@ -14,6 +14,42 @@ class ChallengeService{
             });
         });
     }
+
+    async getChallenge(challengeId) {
+        return new Promise((resolve, reject) => {
+            db.query('select title, description, location, challenge_date, points, image_url from challenges where id = ?;',
+                [challengeId]
+                , function (error, result) {
+                    if (error) {
+                        reject(new Error(error));
+                        return;
+                    }
+
+                    let data = result[0];
+
+                    // Now that we have the data, we need to get related badges
+                    data['badges'] = [];
+                    db.query('select b.name, b.image_url from challenges c join challenge_badges cb on c.id = cb.challenge_id ' +
+                        'join badges b on cb.badge_id = b.id where c.id = ?;',
+                        [challengeId]
+                        , function (error, badges) {
+                            if (error) {
+                                reject(new Error(error));
+                                return;
+                            }
+
+                            // Assign badges to the data object
+                            if (badges) {
+                                data['badges'] = badges;
+                            }
+
+                            return resolve(data);
+                        });
+                });
+
+
+        });
+    }
 }
 
 module.exports = ChallengeService;
