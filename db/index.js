a7546d75f1dbe62e86469e98efc3e7771ace9415
Nicolas Pascal <npascal@octo.com>
2019-07-06 08:12:25 +1000

    Use postgres db

--- db/index.js
+++ db/index.js
@@ -1,13 +1,28 @@
-const mysql = require('mysql');
+// const mysql = require('mysql');
+
+// const dotenv = require('dotenv');
+// dotenv.config({path: '.env'});
+
+// const db = mysql.createConnection ({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DATABASE
+// });
+
+// global.db = db;
+
+const { Client } = require('pg');
 
 const dotenv = require('dotenv');
 dotenv.config({path: '.env'});
 
-const db = mysql.createConnection ({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+console.log('DB_URL'+ process.env.DB_URL)
+const client = new Client({
+  connectionString: process.env.DB_URL,
+  ssl: true,
 });
 
-global.db = db;
+client.connect();
+
+global.db = client;
