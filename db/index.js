f36600b6c617e24a2f1a8bc83561ffefd1dc5ef3
Lorenzo Rodrigo <lorenzo.rodrigo@4mation.com.au>
2019-07-10 17:52:43 +1000

    Added postgress connection pool

--- db/index.js
+++ db/index.js
@@ -1,23 +1,12 @@
-const {Client} = require('pg');
+const { Pool, Client } = require('pg');
 
 const dotenv = require('dotenv');
 dotenv.config({path: '.env'});
 
-global.db = {
-    query: async (query, values, callback) => {
-        const client = new Client({
-            connectionString: process.env.DATABASE_URL,
-            ssl: true,
-        });
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+});
 
-        client.connect().then(() => {
-            client.query(query, values, function (err, res) {
-                try {
-                    return callback(err, res)
-                } finally {
-                    client.end()
-                }
-            })
-        })
-    }
-};
+global.db = pool;
+
+module.exports = pool;
