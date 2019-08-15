f5c2fb6cc1ca2b31cf8b5997dccb6bebb65464d8
Nicolas Pascal <npascal@octo.com>
2019-08-15 17:35:30 +1000

    Add Login e2e test

---
+++ test/e2e/setup.sql
@@ -0,0 +1,36 @@
+-- Delete Tables
+drop table if exists users;
+drop table if exists companies;
+
+-- Create tables
+
+-- Companies
+create table if not exists companies
+(
+  id        serial not null
+    constraint companies_pkey
+    primary key,
+  name      varchar(255),
+  url       varchar(255),
+  image_url varchar(255)
+);
+
+-- Users
+create table if not exists users
+(
+  id  serial  not null
+    constraint users_pkey
+    primary key,
+  full_name  varchar(255),
+  email      varchar(255) not null unique,
+  title      varchar(255),
+  image_url  varchar(255),
+  company_id integer
+    constraint users_company
+    references companies
+    on delete cascade,
+  password   varchar(255) not null
+);
+
+-- Insert Data
+INSERT INTO users (full_name, email, title, image_url, password) VALUES ('Gandalf The Grey', 'gandalf@theshire.com', 'Wizard', 'https://uncledanny1979.files.wordpress.com/2010/03/gandalf.jpg', MD5('You should not pass'));
--- test/e2e/test.js
+++ test/e2e/test.js
@@ -1,15 +1,36 @@
-let request = require('supertest');
+const request = require('supertest');
 const server = require('../../index.js');
+const expect = require('chai').expect;
 
-request = request(server);
+//const app = request(server);
 
 describe('Test API', function() {
+    //app.set('Content-Type', 'application/json');
 
     describe('Test endpoint', function () {
         it('should return a 200 Http response', async function () {
-            await request
+            await request(server)
                 .get('/hello/test')
                 .expect(200)
         })
+    });
+
+    describe('Login Endpoint', function () {
+        it('should return 200 Http response and user id if login successful', async function () {
+            await request(server)
+                .post('/login')
+                .send({
+                    email: 'gandalf@theshire.com',
+                    password: 'You should not pass',
+                })
+                .set('Content-Type', 'application/json')
+                .expect(200)
+                .then(res => {
+                    expect(res.body).to.have.property('id');
+                    expect(res.body.id).to.be.an('Number');
+                })
+        })
+
+        //TODO: Test unauthorised request
     });
 });
