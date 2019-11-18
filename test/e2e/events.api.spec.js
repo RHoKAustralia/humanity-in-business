06b755cebc3ee5de7d1893a98f4ca8696cdb190f
Nicolas Pascal <nicolas.pascal.job@gmail.com>
2019-11-18 19:44:02 +1100

    Add event members endpoint

--- test/e2e/events.api.spec.js
+++ test/e2e/events.api.spec.js
@@ -1,6 +1,6 @@
 const request = require('supertest');
 const server = require('../../src/index.js');
-const expect = require('chai').expect
+const expect = require('chai').expect;
 
 const TeamService = require('../../src/services/TeamService.js');
 const teamService = new TeamService();
@@ -29,6 +29,32 @@ describe('Events API', function () {
         })
     });
 
+    describe('Get events teams', function () {
+        it('should return 200 Http response and events members if any', async function () {
+            await request(server)
+                .get('/events/1/members')
+                .set('Content-Type', 'application/json')
+                .expect(200)
+                .then(res => {
+                    expect(res.body).to.be.an('Array');
+                    expect(res.body).to.have.lengthOf(1);
+                    expect(res.body[0]).to.deep.equal(
+                    {
+                        user_id: 1,
+                        full_name: 'Gandalf The Grey',
+                        title: 'Wizard',
+                        image_url: 'https://uncledanny1979.files.wordpress.com/2010/03/gandalf.jpg',
+                        team_id: 1,
+                        project_id: 1,
+                        project_name: 'Save the Middle Earth',
+                        project_owner: 'The Fellowship of the Ring',
+                        project_description: 'Destroy the ring',
+                        project_image_url: 'http://lotr.org/rivendell.jpg',
+                    })
+                })
+        })
+    });
+
     describe('User join an team', function () {
         it('should return 201 Http response', async function () {
             await request(server)
@@ -37,7 +63,7 @@ describe('Events API', function () {
                     user_id: 2
                 })
                 .set('Content-Type', 'application/json')
-                .expect(201)
+                .expect(201);
 
             await request(server)
                 .get('/teams/1/members')
