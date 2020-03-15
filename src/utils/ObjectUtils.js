9a6a937d7eaf2634b322fe6b3681d78f584dd8a7
Nicolas Pascal <nicolas.pascal.job@gmail.com>
2020-03-15 20:20:27 +1100

    Fix object util failing with Node 10

--- src/utils/ObjectUtils.js
+++ src/utils/ObjectUtils.js
@@ -1,8 +1,9 @@
 class ObjectUtils {
     prefixAllProperties(object, prefix) {
-        return Object.fromEntries(
-            Object.entries(object).map(([k, v]) => [`${prefix}${k}`, v])
-        )
+        let obj = {};
+        Object.entries(object).map(([k, v]) => [`${prefix}${k}`, v])
+            .forEach(([k, v]) => obj[k] = v);
+        return obj;
     }
 }
 
